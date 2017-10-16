605af917c30ff3419a69055b97e57600617d7e23
tarasewiczrafal <32269516+tarasewiczrafal@users.noreply.github.com>
2017-10-16 17:09:56 +0200

    cos2

--- cwiczenia32/main.cpp
+++ cwiczenia32/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main()
+struct Probka
 {
-    cout << "Hello world!" << endl;
+    double t;
+    double x;
+    Probka(double _t, double _x)
+    {
+        t= -t;
+        x=-x;
+    }
+};
+
+
+vector<Probka> wczytaj(string nazwa)
+{
+vector<Probka> tab;
+ifstream plik(nazwa);
+
+string linia;
+while(getline(plik,linia))
+{
+    cout<<linia<<endl;
+}
+    plik.close();
+
+    return tab;
+}
+int main(int argc, char* argv[])
+{
+    if(argc!=2)
+    {
+        return -1;
+    }
+    cout<< argv[1]<<endl;
+    string nazwa_pliku=argv[1];
+
+    vector <Probka> dane = wczytaj(nazwa_pliku);
+cin.get();
     return 0;
 }
