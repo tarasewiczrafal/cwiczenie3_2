2cf33eb3d934d67a82651e79e6d2c1f89d5b7857
tarasewiczrafal <32269516+tarasewiczrafal@users.noreply.github.com>
2017-10-16 17:21:33 +0200

    cos3

--- cwiczenia32/main.cpp
+++ cwiczenia32/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <stdexcept>
 using namespace std;
 
 struct Probka
@@ -15,6 +17,11 @@ struct Probka
     }
 };
 
+//vector<Probka> tablica;
+//tablica push_back(p1);
+//cout<<tablica[0].t;
+
+
 
 vector<Probka> wczytaj(string nazwa)
 {
@@ -24,22 +31,41 @@ ifstream plik(nazwa);
 string linia;
 while(getline(plik,linia))
 {
-    cout<<linia<<endl;
+    stringstream aa(linia);
+    double _t, _x;
+    aa>>_t;
+    aa.ignore();
+    aa>>_x;
+    cout<<_t<<endl;
+    cout<<_x<<endl;
+    tab.push_back(Probka(_t,_x));
 }
     plik.close();
-
     return tab;
 }
+void zapis(vector<Probka>dane, string nazwa)
+{
+    ofstream zapisanie (nazwa);
+    for(int i=0; i<dane.size();i++)
+    {
+        zapisanie<<dane[i].t<<endl;
+
+    }
+    zapisanie.close();
+}
+
+
 int main(int argc, char* argv[])
 {
     if(argc!=2)
     {
         return -1;
     }
+
     cout<< argv[1]<<endl;
     string nazwa_pliku=argv[1];
-
     vector <Probka> dane = wczytaj(nazwa_pliku);
-cin.get();
+    zapis(dane,  "zapis.txt");
+    cin.get();
     return 0;
 }
